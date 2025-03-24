30c153c500137d196dd78619672c9021c6baae6c
BuyMyMojo <hello+git@buymymojo.net>
2025-03-24 23:43:25 +1100

    Change to the latest kernel

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -26,6 +26,9 @@ in
     ./hardware-configuration.nix
   ];
 
+  # Use the latest linux Kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
