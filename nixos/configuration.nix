f26253278bc65692a33e58e3b259ae46d2326766
BuyMyMojo <hello+git@buymymojo.net>
2025-03-24 23:53:41 +1100

    Prefer RADV driver

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -341,6 +341,11 @@ in
     '';
   };
 
+  # === Prefer RADV driver ===
+  environment.variables = {
+    AMD_VULKAN_ICD = "RADV";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
