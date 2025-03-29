9e2914d0dc03be454063adc940a4f09d26a1b4aa
BuyMyMojo <hello+git@buymymojo.net>
2025-03-30 02:49:39 +1100

    Format services.nix

--- nixos/services.nix
+++ nixos/services.nix
@@ -35,55 +35,52 @@
     };
 
     syncthing = {
-        enable = true;
-        group = "users";
-        user = "buymymojo";
-        dataDir = "/home/buymymojo/Documents/Syncthing";    # Default folder for new synced folders
-        configDir = "/home/buymymojo/Documents/.config/syncthing";   # Folder for Syncthing's settings and keys
+      enable = true;
+      group = "users";
+      user = "buymymojo";
+      dataDir = "/home/buymymojo/Documents/Syncthing"; # Default folder for new synced folders
+      configDir = "/home/buymymojo/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
     };
 
-
-
-
-      # services.blocky = {
-  #   enable = true;
-  #   settings = {
-  #     ports.dns = 53; # Port for incoming DNS Queries.
-  #     upstreams.groups.default = [
-  #       "https://one.one.one.one/dns-query" # Using Cloudflare's DNS over HTTPS server for resolving queries.
-  #     ];
-  #     # For initially solving DoH/DoT Requests when no system Resolver is available.
-  #     bootstrapDns = {
-  #       upstream = "https://one.one.one.one/dns-query";
-  #       ips = [ "1.1.1.1" "1.0.0.1" ];
-  #     };
-  #     #Enable Blocking of certian domains.
-  #     blocking = {
-  #       blackLists = {
-  #         #Adblocking
-  #         ads = [
-  #           "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-  #           "https://adaway.org/hosts.txt"
-  #           "https://v.firebog.net/hosts/AdguardDNS.txt"
-  #           ];
-  #         #Another filter for blocking adult sites
-  #         adult = ["https://blocklistproject.github.io/Lists/porn.txt"];
-  #         #You can add additional categories
-  #       };
-  #       #Configure what block categories are used
-  #       clientGroupsBlock = {
-  #         default = [ "ads" ];
-  #         # kids-ipad = ["ads" "adult"];
-  #       };
-  #     };
-  #     customDNS = {
-  #       customTTL = "1h";
-  #       mapping = {
-  #         "upload.aria.coffee" = "192.168.20.2";
-  #       };
-  #     };
-  #   };
-  # };
+    # services.blocky = {
+    #   enable = true;
+    #   settings = {
+    #     ports.dns = 53; # Port for incoming DNS Queries.
+    #     upstreams.groups.default = [
+    #       "https://one.one.one.one/dns-query" # Using Cloudflare's DNS over HTTPS server for resolving queries.
+    #     ];
+    #     # For initially solving DoH/DoT Requests when no system Resolver is available.
+    #     bootstrapDns = {
+    #       upstream = "https://one.one.one.one/dns-query";
+    #       ips = [ "1.1.1.1" "1.0.0.1" ];
+    #     };
+    #     #Enable Blocking of certian domains.
+    #     blocking = {
+    #       blackLists = {
+    #         #Adblocking
+    #         ads = [
+    #           "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+    #           "https://adaway.org/hosts.txt"
+    #           "https://v.firebog.net/hosts/AdguardDNS.txt"
+    #           ];
+    #         #Another filter for blocking adult sites
+    #         adult = ["https://blocklistproject.github.io/Lists/porn.txt"];
+    #         #You can add additional categories
+    #       };
+    #       #Configure what block categories are used
+    #       clientGroupsBlock = {
+    #         default = [ "ads" ];
+    #         # kids-ipad = ["ads" "adult"];
+    #       };
+    #     };
+    #     customDNS = {
+    #       customTTL = "1h";
+    #       mapping = {
+    #         "upload.aria.coffee" = "192.168.20.2";
+    #       };
+    #     };
+    #   };
+    # };
   };
 
 }
