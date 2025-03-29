03bc3ff44a734114b7fe26911e0ed91338bb895b
BuyMyMojo <hello+git@buymymojo.net>
2025-03-30 00:14:39 +1100

    Add a copy of gamemode and mangohud to steam as extra packages to fix compat

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -171,6 +171,15 @@
 
   programs.steam = {
     enable = true;
+    package = pkgs.steam.override {
+      extraPkgs = (pkgs: with pkgs; [
+        gamemode
+        mangohud
+        # additional packages...
+        # e.g. some games require python3
+      ]);
+    };
+
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
